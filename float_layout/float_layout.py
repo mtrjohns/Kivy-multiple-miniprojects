5e55c9b71da52edae25cce9819eb6c46c9f61546
mtrjohns <mtrj1776@hotmail.co.uk>
2022-05-17 21:34:06 +0100

    float layout with buttons

---
+++ float_layout/float_layout.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+# Allows designation of a specific .kv design file
+Builder.load_file("float_layout.kv")
+
+class MyLayout(Widget):
+    pass
+
+
+class AwesomeApp(App):
+    def build(self):
+        # background colour of default window
+        Window.clearcolor = (1, 1, 1, 1)
+        return MyLayout()
+    
+if __name__ == '__main__':
+    AwesomeApp().run()
