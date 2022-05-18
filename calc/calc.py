7c63ea7ac9f2f0997c2c58c72153f23df05b9f3c
mtrjohns <mtrj1776@hotmail.co.uk>
2022-05-18 18:21:15 +0100

    add, subtract, multiply, subtract, on_press

--- calc/calc.py
+++ calc/calc.py
@@ -13,6 +13,50 @@ Builder.load_file("calc.kv")
 class MyLayout(Widget):
     def clear(self):
             self.ids.calc_input.text = "0"
+            
+    # Create button pressing function
+    def button_press(self, button):
+        # Create variable to contain current text box value
+        prior = self.ids.calc_input.text
+        
+        # is 0 in the input
+        if prior == "0":
+            self.ids.calc_input.text = ""
+            self.ids.calc_input.text = f"{button}"
+        else:
+            self.ids.calc_input.text = f"{prior}{button}"
+            
+    # Addition function
+    def add(self):
+        # Create variable to contain current text box value
+        prior = self.ids.calc_input.text
+        # add a plus sign to the text box
+        self.ids.calc_input.text = f"{prior}+"
+    
+    def subtract(self):
+        # Create variable to contain current text box value
+        prior = self.ids.calc_input.text
+        # add a plus sign to the text box
+        self.ids.calc_input.text = f"{prior}-"
+        
+    def multiply(self):
+        # Create variable to contain current text box value
+        prior = self.ids.calc_input.text
+        # add a plus sign to the text box
+        self.ids.calc_input.text = f"{prior}*"
+        
+    def divide(self):
+        # Create variable to contain current text box value
+        prior = self.ids.calc_input.text
+        # add a plus sign to the text box
+        self.ids.calc_input.text = f"{prior}/"
+        
+    def equals(self):
+        # Create variable to contain current text box value
+        prior = self.ids.calc_input.text
+        # add a plus sign to the text box
+        self.ids.calc_input.text = f"{prior}="
+    
 
 class CalculatorApp(App):
     def build(self):
