a23d403fdddaf9c0083bdb647f5d103f4b3a371f
mtrjohns <mtrj1776@hotmail.co.uk>
2022-05-19 21:58:45 +0100

    Accordion

---
+++ accordian/accordian.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+# Allows designation of a specific .kv design file
+Builder.load_file("accordian.kv")
+
+class MyLayout(Widget):
+    pass
+
+class AwesomeApp(App):
+    def build(self):
+        return MyLayout()
+    
+if __name__ == '__main__':
+    AwesomeApp().run()
