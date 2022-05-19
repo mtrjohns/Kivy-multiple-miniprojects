61b5dae5ee86f559cd9b42f71f4f14ab829aedf1
mtrjohns <mtrj1776@hotmail.co.uk>
2022-05-19 20:52:32 +0100

    sliders

---
+++ slider/slider.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+# Allows designation of a specific .kv design file
+Builder.load_file("slider.kv")
+
+class MyLayout(Widget):
+    def slide_it(self, *args):
+        #print(args)
+        self.slide_text.text = str(int(args[1]))
+        
+        self.slide_text.font_size = str(int(args[1]) * 5)
+
+class AwesomeApp(App):
+    def build(self):
+        return MyLayout()
+    
+if __name__ == '__main__':
+    AwesomeApp().run()
